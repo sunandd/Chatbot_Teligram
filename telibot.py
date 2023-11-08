835bad8c48cb071ce49ac43fd3fd0fcf5a6b6055
sunandd <sunandd92@gmail.com>
2023-11-08 13:36:56 +0530

    apenai integrated with the telibot

--- telibot.py
+++ telibot.py
@@ -4,19 +4,19 @@ import os
 import openai
 
 
-class Referance:
+class Reference:
     """
     a class to store previosly response from chatgpt api
     """
 
     def __init__(self) -> None:
-        self.responce =""
+        self.response =""
 
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-referance = Referance()
+reference = Reference()
 TOKEN = os.getenv("Token_teligram")
 
 Model_name = "gpt-3.5-turbo"
@@ -31,7 +31,7 @@ def clear_past():
     """
     forgeting previous contest
     """
-    referance.responce = ""
+    reference.response = ""
 
 @dispatcher.message_handler(commands=['start'])
 async def welcome(message: types.Message):
@@ -59,6 +59,23 @@ async def help(message: types.Message):
 Thannk you for choseing us, please follow the command-\n /start - to start conversation.\n /clear - clearing previous conversation.\n /help - to get help menu.\nI hope this helps. """
     await message.reply(help_command) 
         
+
+@dispatcher.message_handler()
+async def chatgpt(message: types.Message):
+    """
+    A handler to process the user's input and generate a response using the chatGPT API.
+    """
+    print(f">>> USER: \n\t{message.text}")
+    response = openai.ChatCompletion.create(
+        model = Model_name,
+        messages = [
+            {"role": "assistant", "content": reference.response}, # role assistant
+            {"role": "user", "content": message.text} #our query 
+        ]
+    )
+    reference.response = response['choices'][0]['message']['content']
+    print(f">>> chatGPT: \n\t{reference.response}")
+    await bot.send_message(chat_id = message.chat.id, text = reference.response)
 
 
 
